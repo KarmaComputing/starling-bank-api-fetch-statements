6be2a6ac01514259af308da76ff80a06b938eaf6
chrisjsimpson <chris.j.simpson@live.co.uk>
2021-07-01 18:06:47 +0100

    refactor calculations

--- sum.py
+++ sum.py
@@ -2,27 +2,20 @@ import csv
 
 csv_file = csv.reader(open("2021-03.csv"))
 next(csv_file)
-neg = 0
-pos = 0
-dist = 0
+
+total_income = 0
+total_expense = 0
+
 for row in csv_file:
-    _dist = row[4]
-    try:
-        _dist = float(_dist)
-        if _dist > 0:
-            pos += _dist
-        else:
-            # if _dist <= 0: #4517
-            neg += _dist
-            print(_dist)
-            print(neg)  # this is just for testing
-    except ValueError:
-        _dist = 0
-    dist += _dist
-dist = dist / 3
-print("expenses")
-print("{:.2f}".format(round(neg, 2)))
-print("revenue")
-print("{:.2f}".format(round(pos, 2)))
-print("total")
-print("{:.2f}".format(round(dist, 2)))
+    amount = int(float(row[4]) * 100)
+    if amount < 0:
+        total_expense += amount
+    else:
+        total_income += amount
+breakpoint()
+
+cashflow = total_income - abs(total_expense)
+
+print(f"Expenses: {total_expense / 100}")
+print(f"Revenue: {total_income / 100}")
+print(f"Cashflow: {cashflow / 100}")
